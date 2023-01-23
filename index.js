2664ef53a74fa9c5859df454ef28c25947e7ba04
JuarezRamiro <ramiroakd123@hotmail.com>
2023-01-23 10:22:48 -0300

    segundo commit

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo");
+let yo = {             
+    nombre : "Ramiro",
+    edad : 23,
+    desarrollador : true,
+    date : new Date("july 30 1999"),
+    libro : {
+        nombre : "harry potter",
+        autor : "jepeto",
+        fecha : new Date("march 22 2007"),
+    }
+}
+console.log(yo);
